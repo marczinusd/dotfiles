7793dff7f8616df146d39c2d1c036d132d5a7f9e
David Marczinus <marczinus.david@gmail.com>
2021-10-29 10:34:26 +0200

    add .setup.sh

---
+++ .setup.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+
+pacman -Syyu ripgrep zoxide hyperfine exa fzf git-delta btop bat dust fd procs httpie code lazygit xclip mpv libreoffice-still calibre
+
+pamac install --no-confirm rider webstorm code-features mcfly gitkraken discord spotify onedrive-abraunegg
