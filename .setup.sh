211850ed0e44e6a6789fc307cce95a322b1743ec
David Marczinus <marczinus.david@gmail.com>
2021-10-29 14:42:40 +0200

    add more stuff to setup.sh

--- .setup.sh
+++ .setup.sh
@@ -3,7 +3,27 @@
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-pacman -Syyu ripgrep zoxide hyperfine exa fzf git-delta btop bat dust fd procs httpie zsh-autosuggestions code lazygit xclip mpv libreoffice-still calibre dotnet-sdk
+# OMZ plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+# All the things
+sudo pacman -Syyu ripgrep zoxide hyperfine exa fzf git-delta btop bat dust fd procs httpie nnn code lazygit xclip mpv libreoffice-still calibre dotnet-sdk firefox-developer-edition emacs nodejs
 
 # AUR stuff
-pamac install --no-confirm rider webstorm code-features mcfly gitkraken discord spotify onedrive-abraunegg nnn-icons
+sudo pamac install --no-confirm rider webstorm code-features mcfly gitkraken discord spotify onedrive-abraunegg apple-fonts
+
+# tmux setup
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
+# doom
+git clone https://github.com/marczinusd/.doom.d.git
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
+
+firefox-developer-edition https://github.com/microsoft/cascadia-code/releases
