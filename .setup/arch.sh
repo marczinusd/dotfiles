4d2b5bfc8bbcb93c7603e0f38e031fb576b44f8f
David Marczinus <marczinus.david@gmail.com>
2021-11-09 00:32:50 +0100

    restructure arch setup script

--- .setup/arch.sh
+++ .setup/arch.sh
@@ -1,7 +1,5 @@
 #/bin/sh
 
-sudo sed -Ei '/EnableAUR/s/^#//' /etc/pamac.conf
-
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -10,11 +8,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
-# All the things
-sudo pacman -Syyu ripgrep zoxide hyperfine exa fzf git-delta btop bat dust fd procs httpie hub nnn code lazygit neovim broot xclip mpv libreoffice-still calibre dotnet-sdk firefox-developer-edition emacs nodejs
-
-# AUR stuff
-pamac install --no-confirm rider webstorm webstorm-jre code-features mcfly gitkraken discord spotify onedrive-abraunegg apple-fonts
+# ALL the things
+yay -S ripgrep zoxide hyperfine exa fzf git-delta btop bat dust fd procs httpie hub nnn code code-features lazygit neovim broot xclip mpv libreoffice-still calibre dotnet-sdk firefox-developer-edition emacs nvm rider webstorm webstorm-jre gitkraken discord spotify onedrive-abraunegg apple-fonts
 
 # tmux setup
 git clone https://github.com/gpakosz/.tmux.git
@@ -26,4 +21,9 @@ git clone https://github.com/marczinusd/.doom.d.git
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 
-firefox-developer-edition https://github.com/microsoft/cascadia-code/releases
+# apply dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+git clone git@github.com:marczinusd/dotfiles.git $HOME/cfg
+config checkout
+config config --local status.showUntrackedFiles no
