222f3a87ff6b322820492b500ed2b1497f23b86a
David Marczinus <marczinus.david@gmail.com>
2021-11-09 10:42:36 +0100

    fix xrandr script

--- .config/i3/4k+portrait.sh
+++ .config/i3/4k+portrait.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 3840x120 --rotate left --output
+xrandr --output DisplayPort-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DisplayPort-1 --mode 1920x1080 --pos 3840x120 --rotate left
 
 xrandr --output DisplayPort-0 --auto --output DisplayPort-1 --auto --scale 1.5x1.5 --right-of DisplayPort-0
