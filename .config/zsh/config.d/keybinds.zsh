5ea9617144bb70c4ba23c0e09e758c77b682a467
David Marczinus <marczinus.david@gmail.com>
2021-11-13 17:13:50 +0100

    added (well, copied) amazing ctrl+f widget

--- .config/zsh/config.d/keybinds.zsh
+++ .config/zsh/config.d/keybinds.zsh
@@ -20,5 +20,30 @@ __edit-widget(){
   return $ret
 }
 
+
+
+# Credit goes to https://github.com/junegunn/fzf/issues/1750#issuecomment-671197158 for this absolute banger of a command, slightly modified and uglified by me
+RG_PREFIX='rg --line-number --column --no-heading --color=always --smart-case '
+AG_PREFIX='ag --color --column'
+INITIAL_QUERY=''
+FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY' $HOME"
+
+__fif() {
+    fzf --bind "change:reload:$RG_PREFIX {q} . || true" --ansi --phony --query "$INITIAL_QUERY" --preview 'bat --style=numbers --color=always -r $(($(echo {} | cut -d ":" -f2) - 1)):$(($(echo {} | cut -d ":" -f2) + 1)) --highlight-line $(echo {} | cut -d ":" -f2)  $(echo {} | cut -d ":" -f1)' | cut -d ':' -f1,2,3 | xargs $EDITOR
+}
+
+find-in-files() {
+  LBUFFER="${LBUFFER}$(__fif)"
+  local ret=$?
+  zle reset-prompt
+  return $ret
+}
+
+zle -N find-in-files
+bindkey '^f' find-in-files
+
 zle -N __edit-widget
 bindkey '\ee' __edit-widget
+
+zle -N __ag-widget
+bindkey '\ef' __ag-widget
