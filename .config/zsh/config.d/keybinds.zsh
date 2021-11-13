4fbc066dc15acabb5f6982a120f312ac1273e9a9
David Marczinus <marczinus.david@gmail.com>
2021-11-13 23:04:56 +0100

    small keybind changes

--- .config/zsh/config.d/keybinds.zsh
+++ .config/zsh/config.d/keybinds.zsh
@@ -3,7 +3,7 @@ bindkey -s '\ez' 'zi^M' # zoxide interactive
 # credit to https://stackoverflow.com/a/61494171 for the original implementation
 __edit() (
   setopt localoptions pipefail no_aliases 2> /dev/null
-  local file=$(eval "${FZF_DEFAULT_COMMAND}" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS --preview 'bat --style=numbers --color=always --line-range :500 {}'" $(__fzfcmd) -m "$@" | while read item; do
+  local file=$(eval "${FZF_DEFAULT_COMMAND}" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS --preview 'bat --style=full --color=always --line-range :500 {}'" $(__fzfcmd) -m "$@" | while read item; do
     echo -n "${(q)item}"
   done)
   local ret=$?
@@ -21,7 +21,7 @@ __edit-widget(){
 }
 
 zle -N __edit-widget
-bindkey '\ee' __edit-widget
+bindkey '^e' __edit-widget
 
 # Credit goes to https://github.com/junegunn/fzf/issues/1750#issuecomment-671197158 for this absolute banger of a command, slightly modified and uglified by me
 RG_PREFIX='rg --line-number --column --no-heading --color=always --smart-case '
@@ -29,7 +29,7 @@ INITIAL_QUERY=''
 #FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY' $HOME"
 
 __fif() {
-  fzf --height 40% --layout reverse --bind "change:reload:$RG_PREFIX {q} . || true" --ansi --phony --query "$INITIAL_QUERY" --preview 'bat --style=numbers --color=always -r $(($(echo {} | cut -d ":" -f2) - 1)):$(($(echo {} | cut -d ":" -f2) + 1)) --highlight-line $(echo {} | cut -d ":" -f2)  $(echo {} | cut -d ":" -f1)' | cut -d ':' -f1,2,3 | xargs --no-run-if-empty $EDITOR
+  fzf --height 40% --layout reverse --bind "change:reload:$RG_PREFIX {q} . || true" --ansi --phony --query "$INITIAL_QUERY" --preview 'bat --style=full --color=always -r $(($(echo {} | cut -d ":" -f2) - 1)):$(($(echo {} | cut -d ":" -f2) + 40)) --highlight-line $(echo {} | cut -d ":" -f2)  $(echo {} | cut -d ":" -f1)' | cut -d ':' -f1,2,3 | xargs --no-run-if-empty $EDITOR
 }
 
 find-in-files() {
