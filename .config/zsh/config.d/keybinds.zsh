72a73f408eb310d622b36b9234630554bd8c17fe
David Marczinus <marczinus.david@gmail.com>
2021-11-13 16:00:16 +0100

    small adjustments to edit widget

--- .config/zsh/config.d/keybinds.zsh
+++ .config/zsh/config.d/keybinds.zsh
@@ -1,8 +1,9 @@
 bindkey -s '\ez' 'zi^M' # zoxide interactive
 
+# credit to https://stackoverflow.com/a/61494171 for the original implementation
 __edit() (
   setopt localoptions pipefail no_aliases 2> /dev/null
-  local file=$(eval "${FZF_DEFAULT_COMMAND}" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS --preview 'bat --color=always --line-range :500 {}'" $(__fzfcmd) -m "$@" | while read item; do
+  local file=$(eval "${FZF_DEFAULT_COMMAND}" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS --preview 'bat --style=numbers --color=always --line-range :500 {}'" $(__fzfcmd) -m "$@" | while read item; do
     echo -n "${(q)item}"
   done)
   local ret=$?
