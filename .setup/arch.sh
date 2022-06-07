ff8eb23f31610096aa78d25cd3156eba1b08d560
David Marczinus <marczinusd@users.noreply.github.com>
2022-06-07 18:57:11 +0200

    Add portainer to docker setup

--- .setup/arch.sh
+++ .setup/arch.sh
@@ -61,6 +61,13 @@ yay -S docker
 systemctl enable docker
 systemctl start docker
 sudo usermod -aG docker $USER
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:2.13.1
+
 
 # doom
 git clone https://github.com/marczinusd/.doom.d.git
